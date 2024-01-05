0a7596ce9e8db5e1940ca8d6bf8567ceed95fc3b
Chanawee <Chanawee_P@cmu.ac.th>
2024-01-05 12:00:07 +0700

    Lab 13_2 complete

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -22,23 +22,30 @@ int main()
     return 0;
 }
 
-void updateImage(bool [][M],int s,int x,int y){
-    for()
-        for()
-            if()
-
-}
-/*void showImage(const bool [][M]){
-    for()
-    cout << "-";
-    cout << "\n";
-        for(){
-        cout << "|";
-        for(){
-            if()
-            cout << "*"
-            else cout << " ";
+void updateImage(bool image[][M],int s,int x,int y){
+    for(int n = 0 ; n < N ; n++){
+        for(int m = 0 ; m < M ; m++){
+            if(sqrt(pow((n-x),2)+(pow((m-y),2))) <= s-1)
+            image[n][m] = 1 ;
         }
-    cout << "|\n";
+}
+}
+
+    void showImage(const bool image[][M]){
+    for(int i=0;i<M+2;i++) cout<<"-";
+    cout<<endl;
+    for(int y = 0 ; y < 30 ; y++){
+        cout<<"|";
+        for(int x = 0 ; x < 70 ; x++){
+            if(image[y][x] == 0){
+                cout<<" ";
+            }
+            else if(image[y][x] == 1){
+                cout<<"*";
+            }
+        }
+        cout<<"|"<<endl;
     }
-}*/
+    for(int i=0;i<M+2;i++) cout<<"-";
+    cout<<endl;
+} 
